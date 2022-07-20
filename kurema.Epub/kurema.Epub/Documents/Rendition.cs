9ccd4be95a320cc21193b422c37ffbf3800143ce
kurema <kurema_makoto_software@yahoo.co.jp>
2022-07-20 23:52:30 +0900

    Add Documents/*.cs

---
+++ kurema.Epub/kurema.Epub/Documents/Rendition.cs
@@ -0,0 +1,8 @@
+﻿namespace kurema.Epub.Documents;
+
+public class Rendition
+{
+    //Definition of Rendition:
+    //https://www.w3.org/publishing/epub3/epub-spec.html#dfn-rendition
+
+}
--- kurema.Epub/kurema.Epub/Extensions/Xml/Epub30/package.cs
+++ kurema.Epub/kurema.Epub/Extensions/Xml/Epub30/package.cs
@@ -13,7 +13,7 @@ namespace kurema.Epub.Xml.Epub30.Package
             spine = new spine();
             manifest = new manifest();
             metadata = new opfpackagecontentMetadata();
-            
+
             uniqueidentifier = DefaultUniqueIdentifier;
             version = packageVersion.Item30;
             collection = new collection[0];
@@ -24,7 +24,57 @@ namespace kurema.Epub.Xml.Epub30.Package
     {
         public opfpackagecontentMetadata()
         {
-            Items = new object[0];
+            Items = new IMetadataItem[0];
+        }
+
+        public void AppendItem(IMetadataItem item)
+        {
+            if (Items is null)
+            {
+                Items = new IMetadataItem[] { item };
+                return;
+            }
+            var itemsTmp = this.Items;
+            Array.Resize(ref itemsTmp, Items.Length + 1);
+            itemsTmp[Items.Length - 1] = item;
+            this.Items = itemsTmp;
+        }
+
+        public void UpdateItemOfSameType<T>(T value, Func<T, bool> judge = null) where T : IMetadataItem
+        {
+            if (value is null) goto nomatch;
+            for (int i = 0; i < this.Items.Length; i++)
+            {
+                var item = this.Items[i];
+                if (item is T itemT && (judge?.Invoke(itemT) ?? true))
+                {
+                    Items[i] = value;
+                    return;
+                }
+            }
+        nomatch:;
+            AppendItem(value);
         }
     }
+
+    public interface IMetadataItem { }
+
+    partial class contributor : IMetadataItem { }
+    partial class coverage : IMetadataItem { }
+    partial class creator : IMetadataItem { }
+    partial class date : IMetadataItem { }
+    partial class description : IMetadataItem { }
+    partial class format : IMetadataItem { }
+    partial class identifier : IMetadataItem { }
+    partial class language : IMetadataItem { }
+    partial class publisher : IMetadataItem { }
+    partial class relation : IMetadataItem { }
+    partial class rights : IMetadataItem { }
+    partial class source : IMetadataItem { }
+    partial class subject : IMetadataItem { }
+    partial class title : IMetadataItem { }
+    partial class type : IMetadataItem { }
+    partial class link : IMetadataItem { }
+    partial class meta : IMetadataItem { }
+
 }
